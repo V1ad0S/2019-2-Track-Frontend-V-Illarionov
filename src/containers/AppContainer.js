7a3085daf223610602509d49e0458662b49ef056
Vladislav Illarionov <vladislavillarionov32@gmail.com>
2019-11-16 14:34:31 +0300

    BrowserRouter to HashRouter

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import ChatList from '../components/Chatlist';
 import MessageForm from '../components/MessageForm';
 import '../styles/globalStyles.css';
